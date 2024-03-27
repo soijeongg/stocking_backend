56cf49c0c646126e6b5b50c536606679232103e9
norwegianwood_97 <jhchoi1997@gmail.com>
2024-03-28 01:52:17 +0900

    Fix: fixed structure

--- main/src/routes/index.js
+++ main/src/routes/index.js
@@ -1,3 +1,11 @@
 import express from 'express';
+import follow from './follow/follow.router.js';
+import stock from './stock/stock.router.js';
+import rank from './rank/rank.router.js';
+import order from './order/order.router.js';
+
 const router = express.Router();
+router.use('/', [follow, stock, rank]);
+router.use('/order', order);
+
 export default router;
