e46ad8443838732e6c24df658032274962f26a51
jovid_18 <cshcho99@gmail.com>
2024-03-29 12:37:25 +0900

    FEAT: add company check logic

---
+++ main/src/routes/company/company.router.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { prisma } from '../../utils/prisma/index.js';
+import { CompanyRepository } from './company.repository.js';
+import { CompanyService } from './company.service.js';
+import { CompanyController } from './company.controller.js';
+import authMiddleware from '../../middlewares/authMiddleware.js';
+const router = express.Router({ mergeParams: true });
+const companyRepository = new CompanyRepository(prisma);
+const companyService = new CompanyService(companyRepository);
+const companyController = new CompanyController(companyService);
+
+router.get('/company', authMiddleware, companyController.getCompanies);
+
+export default router;
