0b11f0f6fb202eae938b16a6a30b0d511b3dc744
jovid_18 <cshcho99@gmail.com>
2024-04-03 23:09:43 +0900

    FEAT: init selling setting

--- main/src/utils/execution/index.js
+++ main/src/utils/execution/index.js
@@ -12,7 +12,7 @@ async function execution(userId, companyId, orderId, type, quantity, price) {
           },
         });
         if (price && buyer.currentMoney < price * quantity) {
-          throw new Error('돈이 부족합니다.');
+          throw new Error('가지고 있는 돈이 부족합니다.');
         }
         // 판매주문들을 모두 조회
         const totalQuantity = await tx.order.groupBy({
@@ -26,7 +26,7 @@ async function execution(userId, companyId, orderId, type, quantity, price) {
           },
         });
         if (totalQuantity[0].quantity < quantity) {
-          throw new Error('주문 가능한 수량이 부족합니다.');
+          throw new Error(`최대 ${totalQuantity[0].quantity}주까지만 구매할 수 있습니다.`);
         }
         if (!price) price = 1000000000; //시장가 주문
         let buyerOrder; //사용자 주문
@@ -55,10 +55,60 @@ async function execution(userId, companyId, orderId, type, quantity, price) {
             },
           });
         }
-        //여기까지 주문 생성 또는 수정 완료
+        //여기까지 구매 주문 생성 또는 수정 완료
       } else {
         //매도 주문
-        
+        const sellerStock = await tx.stock.findFirst({
+          where: {
+            userId,
+            companyId,
+          },
+        });
+        if (!sellerStock || sellerStock.quantity < quantity) {
+          throw new Error('가지고 있는 주식이 부족합니다.');
+        }
+        // 구매주문들을 모두 조회
+        const totalQuantity = await tx.order.groupBy({
+          where: {
+            companyId: companyId,
+            type: 'buy',
+          },
+          by: ['companyId'], // 또는 필요에 따라 다른 필드로 그룹화
+          _sum: {
+            quantity: true,
+          },
+        });
+        if (totalQuantity[0].quantity < quantity) {
+          throw new Error(`최대 ${totalQuantity[0].quantity}주까지만 판매할 수 있습니다.`);
+        }
+        if (!price) price = 0; //시장가 주문
+        let sellerOrder; //사용자 주문
+        if (!orderId) {
+          sellerOrder = await tx.order.create({
+            data: {
+              userId,
+              companyId,
+              type,
+              quantity,
+              price,
+            },
+          });
+          orderId = sellerOrder.orderId;
+        } else {
+          sellerOrder = await tx.order.update({
+            where: {
+              orderId,
+            },
+            data: {
+              userId,
+              companyId,
+              type,
+              quantity,
+              price,
+            },
+          });
+          //여기까지 판매 주문 생성 또는 수정 완료
+        }
       }
     });
   } catch (err) {
