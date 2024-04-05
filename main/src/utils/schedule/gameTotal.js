b716d9919fe4f797070afaca18cd098b0d99ee07
jovid_18 <cshcho99@gmail.com>
2024-04-05 20:29:16 +0900

    FEAT: revise gameTotal.js

--- main/src/utils/schedule/gameTotal.js
+++ main/src/utils/schedule/gameTotal.js
@@ -2,31 +2,21 @@ import { resetUserMoney, createDummyUser, createCompany, createDummyOrderAndStoc
 import { createDummyEvent } from './gameMiddle.js';
 import { deleteDummyUser, deleteCompany, updateStockToCash, updateRankBoard, updateMMR } from './gameEnd.js';
 
-// 0분에서 5분까지 휴식
-///5분에서 6분까지 게임 세팅
-// 6분에서 11분까지 게임 진행
-// 11분에서 12분까지 게임 정리
-
 async function gameTotal() {
-  console.log('게임 시작 대기 중...');
-  // 게임 시작 대기 (0~5분)
-    await new Promise((resolve) => setTimeout(resolve, 300000));
-
-  // 게임 세팅 (5분)
-  console.log('게임 세팅 중...');
+  console.log('게임 세팅 중...1분뒤 시작입니다!');
   await resetUserMoney();
   await createDummyUser();
   await createCompany();
   await createDummyOrderAndStock();
 
-  // 1분 대기 후 게임 진행 시작
+  // 1분 대기 후 게임 시작
   await new Promise((resolve) => setTimeout(resolve, 60000));
-
-  console.log('게임 진행 중...');
-  // 게임 진행 (6~11분)
+  // 게임 시작(1분)
+  console.log('게임 시작!');
+  // 게임 이벤트 생성 (5초마다)
   const eventInterval = setInterval(createDummyEvent, 5000);
 
-  // 게임 진행 중지 (11분 후)
+  // 게임 이벤트 중지 (5분 40초)
   await new Promise((resolve) =>
     setTimeout(() => {
       clearInterval(eventInterval);
@@ -36,12 +26,13 @@ async function gameTotal() {
   //20초 대기
   await new Promise((resolve) => setTimeout(resolve, 20000));
 
-  // 게임 정리 (11~12분)
-  console.log('게임 정리 중...');
+  // 게임 정리 (6분)
+  console.log('게임 종료! 결과 정리 중...');
   await deleteDummyUser();
   await deleteCompany();
   await updateStockToCash();
   await updateRankBoard();
   await updateMMR();
+  console.log('게임 종료!');
 }
 export { gameTotal };
