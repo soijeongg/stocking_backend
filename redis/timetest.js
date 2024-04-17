227c9f63b22bb36891cc278afb97858c2be71034
jovid_18 <cshcho99@gmail.com>
2024-04-17 17:30:03 +0900

    FEAT: init redis folder

--- redis/timetest.js
+++
@@ -1,5 +0,0 @@
-const nowDate = new Date();
-const diffTime = nowDate.getTime() - new Date("2024-01-01").getTime();
-console.log(diffTime);
-const minTime = diffTime / 100000000000;
-console.log(minTime);
