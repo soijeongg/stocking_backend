c4808d5998091fb2882f4ff47f3fb1f69f738180
jovid_18 <cshcho99@gmail.com>
2024-04-23 01:21:28 +0900

    FEAT: add health check part at socket/src/app.js

--- socket/src/app.js
+++ socket/src/app.js
@@ -10,7 +10,10 @@ const server = createServer(app);
 const PORT = process.env.PORT || 3000;
 
 setupWebSocketServer(server);
-
+// 로드밸런싱 헬스 체크를 위해 루트 경로에 간단한 텍스트를 응답합니다.
+app.get('/', (req, res) => {
+  res.send('<h1>Stocking Socket</h1>');
+});
 server.listen(PORT, () => {
   console.log(`${PORT} 포트로 소켓 서버가 열렸어요!`);
 });
