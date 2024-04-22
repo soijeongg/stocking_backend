cf3d988ae34e9acf237efb8e9e11457e8c5ea058
jovid_18 <cshcho99@gmail.com>
2024-04-22 21:14:23 +0900

    FEAT: add game folder

---
+++ game/src/utils/orderQueue/index.js
@@ -0,0 +1,67 @@
+import { prisma } from '../prisma/index.js';
+import { execution } from '../executionVer1/index.js';
+class Queue {
+  constructor() {
+    this.items = [];
+    this.isProcessing = false; // 처리 중인지 상태를 나타내는 플래그
+    this.totalProcessingTime = 0; // 총 처리 시간
+    this.processedMessageCount = 0; // 처리된 메시지 수
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+    this.startProcessing(); // 항목 추가 시 처리 시작을 시도
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null; // 큐가 비어있으면 null 반환
+    }
+    return this.items.shift();
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  // 큐의 처리를 시작하는 함수
+  async startProcessing() {
+    if (this.isProcessing) {
+      return; // 이미 처리 중이면 더 이상 진행하지 않음
+    }
+    this.isProcessing = true; // 처리 시작
+
+    while (!this.isEmpty()) {
+      // console.log(this.items.length);
+      const message = this.dequeue();
+
+      const startTime = Date.now(); // 처리 시작 시간
+      await this.processMessage(message);
+      const endTime = Date.now(); // 처리 완료 시간
+
+      const duration = endTime - startTime; // 처리 시간 계산
+      // console.log(`Message processing time: ${duration}ms`); // 처리 시간 로깅
+      this.totalProcessingTime += duration; // 총 처리 시간 업데이트
+      this.processedMessageCount++; // 처리된 메시지 수 업데이트
+      const averageProcessingTime = this.totalProcessingTime / this.processedMessageCount;
+      // console.log(`Average message processing time: ${averageProcessingTime}ms`);
+    }
+
+    this.isProcessing = false; // 모든 항목 처리 완료
+  }
+
+  // 실제 메시지를 처리하는 함수
+  async processMessage(message) {
+    const jsonOrderData = JSON.parse(message);
+    const { orderType, userId, companyId, orderId, type, quantity, price } = jsonOrderData;
+    await execution(orderType, userId, companyId, orderId, type, quantity, price);
+  }
+}
+
+const orderQueue = new Queue();
+
+async function insertOrderMessageQueue(messageString) {
+  orderQueue.enqueue(messageString);
+}
+
+export { insertOrderMessageQueue };
---
+++ game/src/utils/schedule/gameEnd.js
@@ -0,0 +1,156 @@
+import { prisma } from '../prisma/index.js';
+import { sendToMatchingServer } from '../sendToMatchingServer/index.js';
+
+/**
+ * @description
+ * 더미 사용자를 삭제합니다.
+ */
+async function deleteDummyUser() {
+  // 더미 사용자를 삭제
+  try {
+    await prisma.user.deleteMany({
+      where: {
+        dummy: true,
+      },
+    });
+  } catch (err) {
+    console.error('더미 사용자 삭제 중 오류가 발생했습니다:', err);
+  }
+}
+/**
+ * @description
+ * 회사 정보를 삭제합니다.
+ */
+async function deleteCompany() {
+  try {
+    await prisma.company.deleteMany();
+  } catch (err) {
+    console.error('회사 정보를 삭제하는 동안 오류가 발생했습니다:', err);
+  }
+}
+/**
+ * @description
+ * 사용자의 주식을 현금으로 변환하여 총자산을 업데이트합니다.
+ */
+async function updateStockToCash() {
+  try {
+    await prisma.$transaction(async (tx) => {
+      let users = await tx.user.findMany();
+      const companies = await tx.company.findMany();
+      let companyInfo = {};
+      for (const company of companies) {
+        companyInfo[company.companyId] = company.currentPrice;
+      }
+      for (let user of users) {
+        let totalAsset = BigInt(user.currentMoney); // user.currentMoney가 BigInt 호환 값이라고 가정합니다.
+        const stocks = await tx.stock.findMany({
+          where: {
+            userId: user.userId,
+          },
+        });
+        await tx.stock.deleteMany({
+          where: {
+            userId: user.userId,
+          },
+        });
+
+        for (const stock of stocks) {
+          totalAsset += BigInt(companyInfo[stock.companyId]) * BigInt(stock.quantity);
+        }
+        totalAsset = totalAsset.toString(); // BigInt를 문자열로 변환합니다.
+        await tx.user.update({
+          where: {
+            userId: user.userId,
+          },
+          data: {
+            currentMoney: totalAsset,
+            totalAsset: totalAsset,
+          },
+        });
+      }
+    });
+  } catch (err) {
+    console.error('주식을 현금으로 변환하는 동안 오류가 발생했습니다:', err);
+  }
+}
+/**
+ * @description
+ * 사용자의 업데이트된 총자산을 이용하여 랭킹보드를 업데이트합니다.
+ */
+async function updateRankBoard() {
+  try {
+    const users = await prisma.user.findMany();
+    await prisma.rank.deleteMany();
+    users.sort((a, b) => {
+      const aProfitRate = (BigInt(a.totalAsset) - BigInt(a.initialSeed)) / BigInt(a.initialSeed);
+      const bProfitRate = (BigInt(b.totalAsset) - BigInt(b.initialSeed)) / BigInt(b.initialSeed);
+      return Number(bProfitRate - aProfitRate); // BigInt 비교 후, 숫자로 변환
+    });
+    for (let i = 0; i < Math.min(users.length, 5); i++) {
+      let diff = BigInt(users[i].totalAsset) - BigInt(users[i].initialSeed);
+      let rate = (Number(diff) / Number(users[i].initialSeed)) * 100;
+      rate = Math.round(rate * 100) / 100;
+      await prisma.rank.create({
+        data: {
+          userId: users[i].userId,
+          ranking: i + 1,
+          earningRate: rate,
+          nickname: users[i].nickname,
+        },
+      });
+    }
+  } catch (err) {
+    console.error('랭킹보드 업데이트 중 오류가 발생했습니다:', err);
+  }
+}
+/**
+ * @description
+ * 사용자의 MMR을 업데이트합니다.
+ */
+async function updateMMR() {
+  try {
+    const rankUsers = await prisma.rank.findMany();
+    for (let i = 0; i < Math.min(rankUsers.length, 3); i++) {
+      const user = await prisma.user.findUnique({
+        where: {
+          userId: rankUsers[i].userId,
+        },
+      });
+      if (i == 0) {
+        user.mmr += 100;
+      } else if (i == 1) {
+        user.mmr += 50;
+      } else if (i == 2) {
+        user.mmr += 30;
+      }
+      if (user.mmr >= 400) {
+        user.tier = 'diamond';
+      } else if (user.mmr >= 300) {
+        user.tier = 'platinum';
+      } else if (user.mmr >= 200) {
+        user.tier = 'gold';
+      } else if (user.mmr >= 100) {
+        user.tier = 'silver';
+      }
+      await prisma.user.update({
+        where: {
+          userId: user.userId,
+        },
+        data: {
+          mmr: user.mmr,
+          tier: user.tier,
+        },
+      });
+    }
+  } catch (err) {
+    console.error('MMR 업데이트 중 오류가 발생했습니다:', err);
+  }
+}
+async function sendMatchingServerGameEnd() {
+  const jsonData = {
+    reqType: 'gameDelete',
+  };
+  const jsonDataString = JSON.stringify(jsonData);
+  await sendToMatchingServer(jsonDataString);
+}
+export { deleteDummyUser, deleteCompany, updateStockToCash, updateRankBoard, updateMMR, sendMatchingServerGameEnd };
