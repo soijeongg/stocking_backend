7d1eb82a37d8b4b5fb376555f77e4d3318f61081
jovid_18 <cshcho99@gmail.com>
2024-04-27 11:12:14 +0900

    FIX: add time remaing notice for next game

--- game/src/utils/schedule/gameTotal.js
+++ game/src/utils/schedule/gameTotal.js
@@ -55,6 +55,29 @@ const notifyTimeRemaining = () => {
   // 남은 시간 알림
   sendNoticesToAllClients(`게임종료까지 남은 시간 : ${timeRemainingMinutes}분 ${timeRemainingSeconds}초`);
 };
+/**
+ * @description 주기적으로 클라이언트에게 다음 게임 시작까지 남은 시간을 알립니다.
+ * 웹소켓을 사용하여 클라이언트에게 알림을 보냅니다.
+ */
+const notifyTimeRemaining2 = () => {
+  const now = new Date();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const remainder = minutes % 12;
+  // 6분까지 남은 시간 계산
+  const timeUntilNext6Minute = (24 - remainder) % 12;
+  // 남은 시간 계산 (시간과 분)
+  let timeRemainingMinutes = timeUntilNext6Minute - 1;
+  let timeRemainingSeconds = 60 - seconds;
+
+  if (timeRemainingSeconds === 60) {
+    // 정확히 분이 바뀌는 순간, 초를 0으로 설정하고 분을 조정
+    timeRemainingSeconds = 0;
+    timeRemainingMinutes += 1;
+  }
+  // 남은 시간 알림
+  sendNoticesToAllClients(`다음 게임까지 남은 시간 : ${timeRemainingMinutes}분 ${timeRemainingSeconds}초`);
+};
 async function gameTotal() {
   await sendMatchingServerGameEnd();
   await deleteDummyUser(); //더미 유저 삭제
@@ -82,7 +105,7 @@ async function gameTotal() {
       resolve();
     }, 284500)
   );
-  // 남은 시간 공지, 빈 주문 방지 (15초)
+  // 남은 시간 공지, 빈 주문 방지 (14초)
   await new Promise((resolve) =>
     setTimeout(() => {
       clearInterval(noticeInterval);
@@ -99,5 +122,15 @@ async function gameTotal() {
   await deleteCompany(); //회사 삭제
   await updateMMR(); //MMR 업데이트
   await sendMatchingServerGameEnd();
+  sendNoticesToAllClients('다음 게임은 6분뒤에 시작됩니다!'); //다음 게임 시작 알림
+  await waitForMinuteRemainder(7); //7분까지 기다림
+  const noticeInterval2 = setInterval(notifyTimeRemaining2, 15000);
+  // 다음 게임까지 남은 시간 공지 (3000초)
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      clearInterval(noticeInterval2);
+      resolve();
+    }, 290000)
+  );
 }
 export { gameTotal };
