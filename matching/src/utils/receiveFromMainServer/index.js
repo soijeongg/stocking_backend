b2a6dcd94d335a1b2fca16bb4124de3f182d349c
norwegianwood_97 <jhchoi1997@gmail.com>
2024-04-27 13:22:11 +0900

    Fix: matching/execution groupId changed for service & matching.redis changed for service(elastic cache)

--- matching/src/utils/receiveFromMainServer/index.js
+++ matching/src/utils/receiveFromMainServer/index.js
@@ -6,7 +6,7 @@ const kafka = new Kafka({
   clientId: 'matching-server',
   brokers: [`${process.env.KAFKA_IP1}`, `${process.env.KAFKA_IP2}`, `${process.env.KAFKA_IP3}`],
 });
-const consumer = kafka.consumer({ groupId: 'matching-group-local' });
+const consumer = kafka.consumer({ groupId: 'matching-group' });
 
 consumer.on(consumer.events.CONNECT, () => {
   console.log('Kafka Consumer is connected and ready.');
